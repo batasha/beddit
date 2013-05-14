969a42c3307da5f68cd860422ffd0554db1080d1
Natasha and James <natasha+james@appacademy.io>
2013-05-14 09:27:16 -0400

    Initial commit with migrations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beddit::Application.config.secret_token = 'd187cc19909840e109cdcb13cb814e990bce9a6f2816ee9e86c4e3253df3a7e9c2acacf0ebbe59f31f6df5a6ae4f4b5577873f828f868a6101af63eb4e94f864'
