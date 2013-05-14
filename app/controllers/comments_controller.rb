d5eb843fb7dc5bba238a137ac8fc9666451a9c22
Natasha and James <natasha+james@appacademy.io>
2013-05-14 18:14:25 -0400

    Pulling out CommentAncestry to do nesting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,11 +8,14 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
     @comment.save!
 
+    CommentAncestry.create_ancestry(@comment.parent, @comment)
+
     redirect_to post_url(@comment.post)
   end
 
   def show
     @comment = Comment.find(params[:id])
     @child = @comment.children.build
+    @replies = CommentAncestry.where(:ancestor_id => @comment.id).includes(:descendant).all
   end
 end
--- app/models/comment_ancestry.rb
+++ app/models/comment_ancestry.rb
@@ -3,4 +3,13 @@ class CommentAncestry < ActiveRecord::Base
 
   belongs_to :ancestor, :class_name => "Comment"
   belongs_to :descendant, :class_name => "Comment"
+
+  def self.create_ancestry(ancestor, comment)
+    CommentAncestry.create!(ancestor_id: ancestor.id,
+                            descendant_id: comment.id)
+
+    return if ancestor.parent_id.nil?
+
+    CommentAncestry.create_ancestry(ancestor.parent, comment)
+  end
 end
